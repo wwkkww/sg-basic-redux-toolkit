b15fa27c88e7ac107a978e4c52371fb964856b8e
kevin <wongkokwah@gmail.com>
2024-05-10 12:50:00 +0800

    init commit. implement add and remove slice object

---
+++ src/store/index_before_refactor.js
@@ -0,0 +1,85 @@
+import { configureStore, createSlice, createAction } from "@reduxjs/toolkit";
+
+
+export const reset = createAction("app/reset");
+console.log(reset());
+
+const songsSlice = createSlice({
+  name: "song",
+  initialState: [],
+  reducers: {
+    // follow pattern name of slice + / + name of function
+    // eg: song/addSong
+    addSong: (state, action) => {
+      state.push(action.payload);
+    },
+    removeSong: (state, action) => {
+      /**
+       * ! NOTE: In this case, we are using an immutability helper. 
+       * So, splice would be preferred as it is more performant and directly modifies the array.  
+       * The filter method would make a copy of the array with the movie removed.
+       */
+      // return state.filter((song) => song !== action.payload)
+
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+  // use to watch for additional action type
+  extraReducers(builder) {
+    builder.addCase(reset, (state, action) => {
+      return [];
+    })
+  }
+});
+
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie: (state, action) => {
+      state.push(action.payload);
+    },
+    removeMovie: (state, action) => {
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    }
+  },
+  extraReducers(builder) {
+    builder.addCase(reset, (state, action) => {
+      return [];
+    })
+  }
+});
+
+const store = configureStore({
+  //! NOTE: reducer without "s"
+  reducer: {
+    //! NOTE: reducer (without s) is a function that wrap all the "reducers" from the slice
+    songs: songsSlice.reducer,
+    movies: moviesSlice.reducer,
+  },
+});
+
+// console.log(songsSlice.actions);
+// console.log(songsSlice.actions.addSong("New song"));
+
+// const startingState = store.getState()
+// console.log(startingState)
+
+// OPTION 1: dispatch the action with manual type
+// store.dispatch({
+//   type: "song/addSong", // this type follow the reducer pattern from slice
+//   payload: "Imagine",
+// });
+
+// OPTION 2: dispatch the action with the action creator
+// store.dispatch(songsSlice.actions.addSong("Hey Jude"));
+
+const finalState = store.getState();
+console.log(finalState);
+
+// export const songActions = songsSlice.actions;
+export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
+export { store };
